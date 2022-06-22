3844879fbf6e54e0e3a2b581e12a02996d22ff01
Guest 03 <guest03@alunos.sc.senac.br>
2022-06-22 20:38:06 -0300

    Desafio tsc e exemplos

--- .React Native/exercicios/aula2106/scripts/desafio2206/imprimirdesafio.js
+++ .React Native/exercicios/aula2106/scripts/desafio2206/imprimirdesafio.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const desafio_1 = __importDefault(require("./desafio"));
 var p1 = new desafio_1.default(1, "jose", 996994643, "jose@gmail.com", 60, 1.67);
-console.log(p1);
+// console.log(p1)
 p1.salvar();
 p1.validar();
 p1.imprimeDados();
